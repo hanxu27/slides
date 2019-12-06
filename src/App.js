3fc3e6cac712b7be0286bc37f30ee3d6652f772a
Han Xu <hanxu27@gmail.com>
2019-12-06 14:24:24 -0500

    good to go?:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import allImages from "./resources/images";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  //current image plus client specific images
+  state = { image: null, appImages: allImages };
+  componentDidMount() {
+    this.randomImage();
+  }
+
+  //put the image selected into state.image, then remove that image from state.appImage
+  randomImage = () => {
+    let index = this.randomIndex();
+    let tempImages = [].concat(this.state.appImages);
+    let currentImage = tempImages.splice(index, 1);
+    this.setState({ image: currentImage[0] });
+    if (tempImages.length > 0) {
+      this.setState({ appImages: tempImages });
+    } else {
+      this.setState({ appImages: allImages });
+    }
+  };
+
+  // generate the random index based on appImages
+  randomIndex = () => Math.floor(Math.random() * this.state.appImages.length);
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.image !== null && (
+          <header className="App-header">
+            <h2 className="App-headline">{this.state.image.headline}</h2>
+            <img
+              src={this.state.image.source}
+              className="App-picture"
+              alt={this.state.image.headline}
+            />
+            <button onClick={e => this.randomImage()} className="Button-next">
+              Next
+            </button>
+          </header>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/resources/images.js
@@ -0,0 +1,40 @@
+//array of images
+const allImages = [
+  {
+    id: 1,
+    headline: "This dog wearing gloves",
+    source:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=671,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.09.32-AM.png"
+  },
+  {
+    id: 2,
+    headline: "This tree wearing underwear",
+    source:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=675,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.09.39-AM.png"
+  },
+  {
+    id: 3,
+    headline: "This woman with no legs",
+    source:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=718,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.09.48-AM.png"
+  },
+  {
+    id: 4,
+    headline: "This woman with extra long legs",
+    source:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=675,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.09.58-AM.png"
+  },
+  {
+    id: 5,
+    headline: "This kid with no head",
+    source:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=535,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.10.07-AM.png"
+  },
+  {
+    id: 6,
+    headline: "This bird cocktail",
+    source:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=571,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.10.18-AM.png"
+  }
+];
+export default allImages;
